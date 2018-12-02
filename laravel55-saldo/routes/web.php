1cfcc843133ead75b66e9703ffacd4e5bd7235b7
LeandroCGMS <leandrocgms2015@gmail.com>
2018-12-02 20:08:28 -0300

    #12 - Organizar Aplicação Laravel 5.5

--- laravel55-saldo/routes/web.php
+++ laravel55-saldo/routes/web.php
@@ -10,11 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+$this->group(['middleware' => ['auth'], 'namespace' => 'Admin'], function(){
+    $this->get('admin', 'AdminController@index')->name('admin.home');
 });
+
+$this->get('/', 'Site\SiteController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
