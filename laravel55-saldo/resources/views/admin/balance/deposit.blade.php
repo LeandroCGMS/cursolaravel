472efbd0b4bab89a0999e515156703c6c75e9e12
LeandroCGMS <leandrocgms2015@gmail.com>
2018-12-05 10:38:04 -0300

    #19 - Preparar Para Fazer Recarga Saldo do Usuário no Laravel

--- laravel55-saldo/resources/views/admin/balance/deposit.blade.php
+++ laravel55-saldo/resources/views/admin/balance/deposit.blade.php
@@ -20,7 +20,7 @@
             <form method="POST" action="{{ route('deposit.store') }}">
             {!! csrf_field() !!}
                 <div class="form-group">
-                    <input type="text" placeholder="Valor Recarga" class="form-control">
+                    <input type="text" name="value" placeholder="Valor Recarga" class="form-control">
                 </div>
                 <div class="form-group">
                     <button type="submit" class="btn btn-success">Recarregar</button>
