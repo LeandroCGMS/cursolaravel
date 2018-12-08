e9d0a3b9c82d0ddd902bdb463e058b2913d98406
LeandroCGMS <leandrocgms2015@gmail.com>
2018-12-08 17:56:42 -0300

    #5 Curso de Laravel 5.3 - Routes Final

--- laravel53-basico/routes/web.php
+++ laravel53-basico/routes/web.php
@@ -1,5 +1,29 @@
 <?php
 
+Route::group(['prefix' => 'painel', 'middleware' => 'auth'], function(){
+    Route::get('/users', function(){
+        return 'Controle de Usuários';
+    });
+    Route::get('/financeiro', function(){
+        return 'Financeiro Painel';
+    });
+    Route::get('/', function(){
+        return '<h1>Dashboard do Painel</h1>';
+    });
+});
+
+Route::get('login', function(){
+    return '<h1>Página de Formulário Fictício de Login</h1>';
+});
+
+Route::get('/categoria2/{idCat?}', function($idCat='valor padrão'){
+    return "Posts da categoria {$idCat}";
+});
+
+Route::get('/categoria/{idCategoria}/nome-fixo/{prm2}', function($idCat, $prm2){
+    return "Posts da categoria {$idCat} - {$prm2}";
+});
+
 Route::get('nome1/nome2/nome7', function(){
     return 'Rota grande';
 })->name('rota.nomeada');;
