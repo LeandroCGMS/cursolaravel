239fb4ba92aaa8844468210022bad74aebb3527b
LeandroCGMS <leandrocgms2015@gmail.com>
2018-12-08 12:01:33 -0300

    #4 Curso de Laravel 5.3 - Rotas

--- laravel53-basico/routes/web.php
+++ laravel53-basico/routes/web.php
@@ -1,16 +1,29 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('nome1/nome2/nome7', function(){
+    return 'Rota grande';
+})->name('rota.nomeada');;
+
+Route::any('/any', function(){
+    return 'Route Any';
+});
+
+Route::match(['get', 'post'], '/match', function(){
+   return 'Route match' ;
+});
+
+Route::post('/post', function() {
+   return 'Route Post';
+});
+
+Route::get('/contato', function(){
+   return "Contato";
+});
+
+Route::get('/empresa',function(){
+    return view('empresa');
+});
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('rota.nomeada');
 });
